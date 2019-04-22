e7ae7d8c8eec13ba103a05891a1723bd3ec3ad31
Alex Karpitski <alex.karpitski@gmail.com>
2019-04-22 18:26:55 +0300

    gcp - query and get kms keyring resource

--- tools/c7n_gcp/c7n_gcp/resources/kms.py
+++ tools/c7n_gcp/c7n_gcp/resources/kms.py
@@ -11,8 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import re
+
 from c7n_gcp.provider import resources
-from c7n_gcp.query import QueryResourceManager, TypeInfo
+from c7n_gcp.query import QueryResourceManager, TypeInfo, ChildResourceManager, ChildTypeInfo
 
 
 @resources.register('kms-location')
@@ -32,4 +35,36 @@ class KmsLocation(QueryResourceManager):
         def get(client, resource_info):
             name = 'projects/{}/locations/{}'.format(
                 resource_info['project_id'], resource_info['location'])
+            return client.execute_command('get', {'name': name})
+
+
+@resources.register('kms-keyring')
+class KmsKeyRing(ChildResourceManager):
+
+    def _get_parent_resource_info(self, child_instance):
+        param_re = re.compile('projects/(.*?)/locations/(.*?)/keyRings/.*?')
+        project_id, location = param_re.match(child_instance['name']).groups()
+        return {'project_id': project_id,
+                'location': location}
+
+    class resource_type(ChildTypeInfo):
+        service = 'cloudkms'
+        version = 'v1'
+        component = 'projects.locations.keyRings'
+        enum_spec = ('list', 'keyRings[]', None)
+        scope = None
+        id = 'name'
+        parent_spec = {
+            'resource': 'kms-location',
+            'child_enum_params': [
+                ('name', 'parent')
+            ]
+        }
+
+        @staticmethod
+        def get(client, resource_info):
+            name = 'projects/{}/locations/{}/keyRings/{}' \
+                .format(resource_info['project_id'],
+                        resource_info['location'],
+                        resource_info['key_ring_id'])
             return client.execute_command('get', {'name': name})
