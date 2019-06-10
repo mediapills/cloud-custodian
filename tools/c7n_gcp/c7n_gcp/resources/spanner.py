320a3acba78c770fb4a1604eb46675016d115ba1
Yauhen Shaliou <yauhen_shaliou@epam.com>
2019-06-10 11:31:18 +0300

    Spanner. Updated delete action for instances, implemented patch action

--- tools/c7n_gcp/c7n_gcp/resources/spanner.py
+++ tools/c7n_gcp/c7n_gcp/resources/spanner.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import re
+from c7n.exceptions import PolicyValidationError
 from c7n.utils import type_schema
 from c7n_gcp.actions import MethodAction
 from c7n_gcp.provider import resources
@@ -48,6 +49,38 @@ class SpannerInstanceDelete(SpannerInstanceAction):
 
     schema = type_schema('delete')
     method_spec = {'op': 'delete'}
+
+
+@SpannerInstance.action_registry.register('patch')
+class SpannerInstancePatch(SpannerInstanceAction):
+
+    schema = type_schema('patch', attributes={'nodeCount': 'object'})
+    method_spec = {'op': 'patch'}
+
+    FIELDS_TO_UPDATE = ['name', 'config', 'displayName', 'nodeCount', 'state', 'labels']
+
+    def validate(self):
+        valid = False
+        for field in self.data.keys():
+            if field in self.FIELDS_TO_UPDATE:
+                valid = True
+                break
+
+        if not valid:
+            raise PolicyValidationError("Nothing to update")
+
+    def get_resource_params(self, model, resource):
+        result = super(SpannerInstancePatch, self).get_resource_params(model, resource)
+        field_mask = []
+        result['body'] = {}
+        result['body']['instance'] = {}
+        for field in self.data.keys():
+            if field in self.FIELDS_TO_UPDATE:
+                field_mask.append(field)
+                result['body']['instance'][field] = self.data[field]
+
+        result['body']['field_mask'] = ', '.join([x for x in field_mask])
+        return result
 
 
 @resources.register('spanner-database-instance')
