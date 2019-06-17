c91df793e572ae4911efd0ab8ad6465b387c861d
Alex Karpitski <aliaksei_karpitski@epam.com>
2019-06-17 11:42:05 +0000

    gcp - gce node groups resource initial setup

---
+++ docs/source/gcp/examples/gce-node-group.rst
@@ -0,0 +1,17 @@
+Compute Engine - Delete Node Groups with Invalid Number of Nodes
+================================================================
+Custodian can delete Node Groups that have too many nodes.
+
+In the example below, the policy checks if there are Node Groups whose ``size`` is greater than ``2``.
+
+.. code-block:: yaml
+    policies:
+        - name: gcp-gce-node-group-delete-greater-than-2-nodes
+          resource: gcp.gce-node-group
+          filters:
+            - type: value
+              key: size
+              op: greater-than
+              value: 2
+          actions:
+            - type: delete
--- tools/c7n_gcp/c7n_gcp/resources/compute.py
+++ tools/c7n_gcp/c7n_gcp/resources/compute.py
@@ -14,13 +14,12 @@
 
 import re
 
-from c7n.utils import type_schema
-
 from c7n_gcp.actions import MethodAction
 from c7n_gcp.provider import resources
 from c7n_gcp.query import QueryResourceManager, TypeInfo
 
 from c7n.filters.offhours import OffHour, OnHour
+from c7n.utils import local_session, type_schema
 
 
 @resources.register('instance')
@@ -184,3 +183,204 @@ class DeleteSnapshot(MethodAction):
         # Docs are wrong :-(
         # https://cloud.google.com/compute/docs/reference/rest/v1/snapshots/delete
         return {'project': project, 'snapshot': snapshot_id}
+
+
+@resources.register('gce-node-group')
+class GceNodeGroup(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'compute'
+        version = 'v1'
+        component = 'nodeGroups'
+        enum_spec = ('aggregatedList', 'items.*.nodeGroups[]', None)
+        scope = 'project'
+        id = 'name'
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_command(
+                'get', {'project': resource_info['project_id'],
+                        'zone': resource_info['zone'],
+                        'nodeGroup': resource_info['resourceName'].rsplit('/', 1)[-1]})
+
+
+@GceNodeGroup.action_registry.register('delete')
+class GceNodeGroupDelete(MethodAction):
+    """
+    `Deletes <https://cloud.google.com/compute/docs/reference/rest/v1/nodeGroups/delete>`_
+    a Node Group. The action does not specify any parameters.
+
+    Example:
+    .. code-block:: yaml
+        policies:
+          - name: gcp-gce-node-group-delete
+            resource: gcp.gce-node-group
+            filters:
+              - type: value
+                key: name
+                value: node-group-to-delete
+            actions:
+              - type: delete
+    """
+    schema = type_schema('delete')
+    method_spec = {'op': 'delete'}
+
+    def get_resource_params(self, m, r):
+        project, zone, node_group = re.match(
+            '.*/projects/(.*?)/zones/(.*?)/nodeGroups/(.*)', r['selfLink']).groups()
+        return {'project': project, 'zone': zone, 'nodeGroup': node_group}
+
+
+class GceNodeGroupChangeSizeAction(MethodAction):
+    def _get_project_zone_node_group(self, r):
+        return re.match('.*/projects/(.*?)/zones/(.*?)/nodeGroups/(.*)', r['selfLink']).groups()
+
+    def _get_current_size(self, r):
+        return r['size']
+
+    def _get_target_size(self):
+        return self.data['target-size']
+
+
+@GceNodeGroup.action_registry.register('increase-size')
+class GceNodeGroupIncreaseSize(GceNodeGroupChangeSizeAction):
+    """
+    `Adds Nodes
+    <https://cloud.google.com/compute/docs/reference/rest/v1/nodeGroups/addNodes>`_
+    to a Node Group.
+
+    The required 'target-size' parameter must be greater than the current Node size, otherwise
+    the `target_not_greater_than_current_error` `ValueError` is raised. As it is possible
+    to have a Node Group without nodes (0), the schema specifies the next possible value (1)
+    as the minimal one.
+
+    Behind the scenes, the `_get_additional_node_count` method defines the number of the Nodes
+    to add that will be further used as a parameter in the API call.
+
+    Example:
+    .. code-block:: yaml
+
+        policies:
+            - name: gcp-gce-node-group-increase-size-from-1-to-2
+              resource: gcp.gce-node-group
+              filters:
+                - type: value
+                  key: size
+                  op: greater-than
+                  value: 1
+              actions:
+                - type: decrease-size
+                  target-size: 2
+    """
+    schema = type_schema('increase-size',
+                         **{
+                             'type': {'enum': ['increase-size']},
+                             'additionalProperties': False,
+                             'required': ['target-size'],
+                             'target-size': {
+                                 'type': 'integer',
+                                 'minimum': 1  # if current size is 0
+                             }
+                         })
+    method_spec = {'op': 'addNodes'}
+    min_target_size = 2
+    target_not_greater_than_current_error = 'Target node group size (%d) ' \
+                                            'must be greater than the current (%s)'
+
+    def get_resource_params(self, m, r):
+        project, zone, node_group = self._get_project_zone_node_group(r)
+        return {'project': project, 'zone': zone, 'nodeGroup': node_group,
+                'body': {'additionalNodeCount': self._get_additional_node_count(r)}}
+
+    def _get_additional_node_count(self, r):
+        """
+        Validates `target_size` (T) against the `current_size` (C)
+        and returns the T - C difference.
+
+        :param r: the same as in `get_resource_params`
+        :return: a positive integer
+        :raises ValueError: if T <= C
+        """
+        current_size = self._get_current_size(r)
+        target_size = self._get_target_size()
+        if target_size <= current_size:
+            raise ValueError(self.target_not_greater_than_current_error %
+                             (target_size, current_size))
+        return target_size - current_size
+
+
+@GceNodeGroup.action_registry.register('decrease-size')
+class GceNodeGroupDecreaseSize(GceNodeGroupChangeSizeAction):
+    """
+    `Deletes Nodes
+    <https://cloud.google.com/compute/docs/reference/rest/v1/nodeGroups/deleteNodes>`_
+    from a Node Group.
+
+    The required 'target-size' parameter must be smaller than the current Node size, otherwise
+    the `target_not_smaller_than_current_error` `ValueError` is raised. As it is possible
+    to have a Node Group without nodes, the schema specifies 0 as the minimal value.
+
+    Behind the scenes, the `_get_nodes_to_delete` method defines the names of the Nodes to delete
+    that will be further used as a parameter in the API call.
+
+    Example:
+    .. code-block:: yaml
+
+        policies:
+            - name: gcp-gce-node-group-decrease-size-from-2-to-1
+              resource: gcp.gce-node-group
+              filters:
+                - type: value
+                  key: size
+                  op: greater-than
+                  value: 2
+              actions:
+                - type: decrease-size
+                  target-size: 1
+    """
+    schema = type_schema('decrease-size',
+                         **{
+                             'type': {'enum': ['decrease-size']},
+                             'additionalProperties': False,
+                             'required': ['target-size'],
+                             'target-size': {
+                                 'type': 'integer',
+                                 'minimum': 0  # if current size is 1
+                             }
+                         })
+    method_spec = {'op': 'deleteNodes'}
+    min_target_size = 1
+    target_not_smaller_than_current_error = 'Target node group size (%d) ' \
+                                            'must be smaller than the current (%s)'
+
+    def get_resource_params(self, m, r):
+        project, zone, node_group = self._get_project_zone_node_group(r)
+        return {'project': project, 'zone': zone, 'nodeGroup': node_group,
+                'body': {'nodes': self._get_nodes_to_delete(r)}}
+
+    def _get_nodes_to_delete(self, r):
+        """
+        Validates `target_size` (T) against the `current_size` (C), calculates the difference
+        (D = C - T) and returns the list of the names of the `nodes
+        <https://cloud.google.com/compute/docs/reference/rest/v1/nodeGroups/listNodes>`_
+        except for the D nodes left out from the end of the list.
+
+        :param r: the same as in `get_resource_params`
+        :return: a non-empty list of Node names
+        :raises ValueError: if T >= C
+        """
+        project, zone, node_group = self._get_project_zone_node_group(r)
+        current_size = self._get_current_size(r)
+        target_size = self._get_target_size()
+        if target_size >= current_size:
+            raise ValueError(self.target_not_smaller_than_current_error %
+                             (target_size, current_size))
+        m = self.manager.get_model()
+        session = local_session(self.manager.session_factory)
+        client = self.get_client(session, m)
+        current_nodes = client.execute_command('listNodes', {
+            'project': project,
+            'zone': zone,
+            'nodeGroup': node_group
+        })
+        return [node['name'] for node in current_nodes['items'][(current_size - target_size):]]
