0504917585d61a0cbbbff4af4c4cd38793180561
Alex Karpitski <aliaksei_karpitski@epam.com>
2019-07-12 11:56:58 +0000

    gcp - appengine actions

--- tools/c7n_gcp/c7n_gcp/resources/appengine.py
+++ tools/c7n_gcp/c7n_gcp/resources/appengine.py
@@ -14,9 +14,11 @@
 
 import re
 
+from c7n_gcp.actions import MethodAction
 from c7n_gcp.provider import resources
 from c7n_gcp.query import QueryResourceManager, TypeInfo, ChildResourceManager, ChildTypeInfo
-from c7n.utils import local_session
+
+from c7n.utils import local_session, type_schema
 
 
 @resources.register('app-engine')
@@ -39,6 +41,111 @@ class AppEngineApp(QueryResourceManager):
 
     def get_resource_query(self):
         return {'appsId': local_session(self.session_factory).get_default_project()}
+
+
+class AppEnginePatchServingStatusAction(MethodAction):
+    """
+    `Patches <https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps/patch>`_
+    the 'Serving' setting. Is supposed to be extended to call `_get_resource_params`
+    from the original `get_resource_params` method with the `serving` flag provided
+    which sets 'servingStatus' either to 'SERVING' or 'USER_DISABLED'.
+
+    Apart from that, `schema` is also expected to be specified in subclasses.
+    """
+    method_spec = {'op': 'patch'}
+
+    def _get_resource_params(self, r, serving):
+        serving_status = 'SERVING' if serving else 'USER_DISABLED'
+        return {'appsId': r['id'],
+                'body': {'servingStatus': serving_status},
+                'updateMask': 'serving_status'}
+
+
+@AppEngineApp.action_registry.register('start')
+class AppEngineStart(AppEnginePatchServingStatusAction):
+    """
+    Starts App Engine by extending `AppEnginePatchServingStatusAction`
+
+    .. code-block:: yaml
+
+        policies:
+          - name: gcp-app-engine-start
+            resource: gcp.app-engine
+            actions:
+              - type: start
+    """
+    schema = type_schema('start')
+
+    def get_resource_params(self, m, r):
+        return AppEnginePatchServingStatusAction._get_resource_params(self, r, True)
+
+
+@AppEngineApp.action_registry.register('stop')
+class AppEngineStop(AppEnginePatchServingStatusAction):
+    """
+    Stops App Engine by extending `AppEnginePatchServingStatusAction`
+
+    .. code-block:: yaml
+
+        policies:
+          - name: gcp-app-engine-stop
+            resource: gcp.app-engine
+            actions:
+              - type: stop
+    """
+    schema = type_schema('stop')
+
+    def get_resource_params(self, m, r):
+        return AppEnginePatchServingStatusAction._get_resource_params(self, r, False)
+
+
+@AppEngineApp.action_registry.register('set')
+class AppEnginePatchSettings(MethodAction):
+    """
+    `Patches <https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps/patch>`_
+    the 'Split health checks' and 'Use Container-Optimized OS' settings.
+
+    If a setting is disabled, the key is removed from the resource rather than
+    being marked as `false`.
+
+    The 'split-health-check' flag controls if split health checks ('readinessCheck'
+    and 'livenessCheck' at an app.yaml level) should be used instead of the legacy
+    health checks ('healthCheck')
+
+    The 'use-container-optimised-os' flag enables the use if Container-Optimized OS
+    base image for VMs, rather than a base Debian image. Before patching the setting,
+    make sure it is enabled for your project, otherwise the update `operation
+    <https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/apps.operations/list>`_
+    will fail although the policy will show no errors.
+
+    .. code-block:: yaml
+
+        policies:
+          - name: gcp-app-engine-set
+            resource: gcp.app-engine
+            actions:
+              - type: set
+                split-health-checks: true
+                use-container-optimized-os: true
+    """
+    schema = type_schema('set',
+                         **{
+                             'additionalProperties': False,
+                             'split-health-checks': {'type': 'boolean'},
+                             'use-container-optimized-os': {'type': 'boolean'}
+                         })
+    method_spec = {'op': 'patch'}
+
+    def get_resource_params(self, m, r):
+        params = {'appsId': r['id'],
+                  'body': {'featureSettings': {}},
+                  'updateMask': 'featureSettings'}
+        feature_settings = params['body']['featureSettings']
+        if 'split-health-checks' in self.data:
+            feature_settings['splitHealthChecks'] = self.data['split-health-checks']
+        if 'use-container-optimized-os' in self.data:
+            feature_settings['useContainerOptimizedOs'] = self.data['use-container-optimized-os']
+        return params
 
 
 @resources.register('app-engine-certificate')
@@ -121,6 +228,34 @@ class AppEngineDomainMapping(ChildResourceManager):
                                            resource_info['resourceName']).groups()
             return client.execute_query('get', {'appsId': apps_id,
                                                 'domainMappingsId': mapping_id})
+
+
+@AppEngineDomainMapping.action_registry.register('delete')
+class AppEngineDomainMappingDelete(MethodAction):
+    """
+    `Deletes <https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1
+    /apps.domainMappings/delete>`_ an App Engine Domain Mapping.
+    The action does not specify any parameters.
+
+    .. code-block:: yaml
+
+        policies:
+          - name: gcp-app-engine-domain-mapping-delete
+            resource: gcp.app-engine-domain-mapping
+            filters:
+              - type: value
+                key: id
+                value: mapping.to.delete
+            actions:
+              - type: delete
+    """
+    schema = type_schema('delete')
+    method_spec = {'op': 'delete'}
+
+    def get_resource_params(self, m, r):
+        apps_id, mapping_id = re.match('apps/(.*?)/domainMappings/(.*)',
+                                       r['name']).groups()
+        return {'appsId': apps_id, 'domainMappingsId': mapping_id}
 
 
 @resources.register('app-engine-firewall-ingress-rule')
--- tools/c7n_gcp/tests/test_appengine.py
+++ tools/c7n_gcp/tests/test_appengine.py
@@ -12,7 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+
 from gcp_common import BaseTest
+from time import sleep
+
+from googleapiclient.errors import HttpError
 
 
 class AppEngineAppTest(BaseTest):
@@ -45,6 +50,78 @@ class AppEngineAppTest(BaseTest):
         resource = policy.resource_manager.get_resource(
             {'resourceName': app_name})
         self.assertEqual(resource['name'], app_name)
+
+    def test_app_start(self):
+        project_id = 'cloud-custodian'
+        app_name = 'apps/' + project_id
+        session_factory = self.replay_flight_data(
+            'app-engine-start', project_id=project_id)
+
+        policy = self.load_policy(
+            {'name': 'gcp-app-engine-start',
+             'resource': 'gcp.app-engine',
+             'actions': [{'type': 'start'}]},
+            session_factory=session_factory)
+
+        resources = policy.run()
+        self.assertEqual(resources[0]['name'], app_name)
+
+        if self.recording:
+            sleep(1)
+
+        app_short_name = resources[0]['id']
+        client = policy.resource_manager.get_client()
+        result = client.execute_query('get', {'appsId': app_short_name})
+        self.assertEqual(result['servingStatus'], 'SERVING')
+
+    def test_app_stop(self):
+        project_id = 'cloud-custodian'
+        app_name = 'apps/' + project_id
+        session_factory = self.replay_flight_data(
+            'app-engine-stop', project_id=project_id)
+
+        policy = self.load_policy(
+            {'name': 'gcp-app-engine-stop',
+             'resource': 'gcp.app-engine',
+             'actions': [{'type': 'stop'}]},
+            session_factory=session_factory)
+
+        resources = policy.run()
+        self.assertEqual(resources[0]['name'], app_name)
+
+        if self.recording:
+            sleep(1)
+
+        app_short_name = resources[0]['id']
+        client = policy.resource_manager.get_client()
+        result = client.execute_query('get', {'appsId': app_short_name})
+        self.assertEqual(result['servingStatus'], 'USER_DISABLED')
+
+    def test_app_set(self):
+        project_id = 'cloud-custodian'
+        app_name = 'apps/' + project_id
+        session_factory = self.replay_flight_data(
+            'app-engine-set', project_id=project_id)
+
+        policy = self.load_policy(
+            {'name': 'gcp-app-engine-set',
+             'resource': 'gcp.app-engine',
+             'actions': [{'type': 'set',
+                          'split-health-checks': True,
+                          'use-container-optimized-os': False}]},
+            session_factory=session_factory)
+
+        resources = policy.run()
+        self.assertEqual(resources[0]['name'], app_name)
+
+        if self.recording:
+            sleep(1)
+
+        app_short_name = resources[0]['id']
+        client = policy.resource_manager.get_client()
+        result = client.execute_query('get', {'appsId': app_short_name})
+        self.assertEqual(result['featureSettings']['splitHealthChecks'], True)
+        self.assertTrue('useContainerOptimizedOs' not in result['featureSettings'])
 
 
 class AppEngineCertificateTest(BaseTest):
@@ -146,6 +223,42 @@ class AppEngineDomainMappingTest(BaseTest):
             {'resourceName': domain_mapping_name})
         self.assertEqual(resource['name'], domain_mapping_name)
         self.assertEqual(resource[parent_annotation_key]['name'], app_name)
+
+    def test_domain_mapping_delete(self):
+        project_id = 'cloud-custodian'
+        app_name = 'apps/' + project_id
+        domain_mapping_id = 'outdated.gcp-li.ga'
+        domain_mapping_name = '{}/domainMappings/{}'.format(app_name, domain_mapping_id)
+        session_factory = self.replay_flight_data(
+            'app-engine-domain-mapping-delete', project_id=project_id)
+
+        policy = self.load_policy(
+            {'name': 'gcp-app-engine-domain-mapping-delete',
+             'resource': 'gcp.app-engine-domain-mapping',
+             'filters': [{
+                 'type': 'value',
+                 'key': 'id',
+                 'value': domain_mapping_id
+             }],
+             'actions': [{'type': 'delete'}]},
+            session_factory=session_factory)
+        parent_annotation_key = policy.resource_manager.resource_type.get_parent_annotation_key()
+
+        resource = policy.run()[0]
+        self.assertEqual(resource['name'], domain_mapping_name)
+        self.assertEqual(resource[parent_annotation_key]['name'], app_name)
+
+        if self.recording:
+            sleep(1)
+
+        client = policy.resource_manager.get_client()
+        app_short_name = project_id
+        try:
+            result = client.execute_query('get', {'appsId': app_short_name,
+                                                  'domainMappingsId': domain_mapping_id})
+            self.fail('found deleted resource: %s' % result)
+        except HttpError as e:
+            self.assertTrue(re.match(".*Domain mapping '.+?' not found.*", str(e)))
 
 
 class AppEngineFirewallIngressRuleTest(BaseTest):
