7c9826eb40d5ce53859d40714ccdd5b268d8e21e
Yauhen Shaliou <yauhen_shaliou@epam.com>
2019-07-30 19:14:43 +0300

    gcp - spanner - fix

--- tools/c7n_gcp/c7n_gcp/actions/__init__.py
+++ tools/c7n_gcp/c7n_gcp/actions/__init__.py
@@ -16,4 +16,4 @@
 from .core import Action, MethodAction
 from .cscc import PostFinding
 from .notify import Notify
-from .iampolicy import SetIamPolicyBaseAction
+from .iampolicy import SetIamPolicy
