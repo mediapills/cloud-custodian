8d42dc59c80a44240bc99857e48499ccd6b16015
Yauhen Shaliou <yauhen_shaliou@epam.com>
2019-08-01 16:30:46 +0300

    gcp - security policy - minor fixes

--- tools/c7n_gcp/c7n_gcp/resources/compute.py
+++ tools/c7n_gcp/c7n_gcp/resources/compute.py
@@ -239,16 +239,8 @@ class GceSecurityPolicyAddRule(MethodAction):
     `Inserts <https://cloud.google.com/compute/docs/reference/rest/v1/securityPolicies/addRule>`_
     a rule into a security policy.
 
-    The `action` specifies the action to perform when the client connection triggers the rule. Can
-
     The 'action' specifies the action to perform when the client connection triggers the rule. Can
     currently be either "allow" or "deny()" where valid values for status are 403, 404, 502.
-
-    The `srcIpRanges` specifies CIDR IP address range.
-
-    The `priority` specifies the priority of a rule in the list.
-
-    :Example:
 
     The 'srcIpRanges' specifies CIDR IP address range.
 
